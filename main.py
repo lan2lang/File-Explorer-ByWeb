1b09fb5466cf9c0a98b7b411b214e3ed3a0d2f4e
四十六时 <58031876+lan2lang@users.noreply.github.com>
2024-05-27 17:56:20 +0800

    新增图片预览功能

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import os
-from waitress import serve
-from flask import Flask, render_template, request, send_from_directory, abort
+from flask import Flask, render_template, send_from_directory, abort, url_for
 
 app = Flask(__name__)
 
@@ -37,5 +36,24 @@ def index(subpath=''):
     # 如果路径既不是文件也不是目录，返回404
     abort(404)
 
+@app.route('/preview/<path:subpath>')
+def preview(subpath):
+    current_path = os.path.join(ROOT_DIR, subpath)
+
+    if not os.path.exists(current_path) or not os.path.isfile(current_path):
+        abort(404)
+
+    file_extension = os.path.splitext(current_path)[1].lower()
+
+    if file_extension in ['.txt', '.md', '.py', '.log']:  # 支持的文本文件类型
+        with open(current_path, 'r', encoding='utf-8') as file:
+            content = file.read()
+        return render_template('preview.html', content=content, file_type='text')
+    elif file_extension in ['.jpg', '.jpeg', '.png', '.gif']:  # 支持的图片文件类型
+        return render_template('preview.html', file_url=url_for('index', subpath=subpath), file_type='image')
+
+    abort(415)  # 不支持的文件类型
+
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
+    # 修改 host 参数为 '0.0.0.0' 以允许局域网访问，端口为8080
+    app.run(debug=False, host='0.0.0.0', port=8080)
